cd2e4a4695120b977b131c3b9e26897e2b861254
loganameres <ljameres@gmail.com>
2025-09-24 11:20:02 -0400

    ReadFile.java class

---
+++ ReadFile.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        File myObj = new File("/Users/loganameres/Desktop/Topic 1/article1.txt");
+
+        try (Scanner myReader = new Scanner(myObj)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+    }
+}
