dea7b9094db30e9c8adb49c0c9808850a5202af7
michaelkobus <mtkobus123@gmail.com>
2025-10-01 11:48:10 -0400

    Moved stop words into the main

--- Main.java
+++ Main.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //Scanner Declaration
         File myObj = new File("/Users/loganameres/Desktop/Topic 1/article1.txt");
 
         try (Scanner myReader = new Scanner(myObj)) {
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+        }
+
+        //Stop words declaration
+        File stopWords = new File("/Users/loganameres/Desktop/Programming Workshop/Semester-Project/stopwords.txt");
+
+        try (Scanner myReader = new Scanner(stopWords)) {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 System.out.println(data);
