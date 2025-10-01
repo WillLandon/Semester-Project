60d36964fec216b12bf641da297f588066fa2217
michaelkobus <mtkobus123@gmail.com>
2025-10-01 11:52:08 -0400

    deadhusikfcjheksa

--- StopWords.java
+++ StopWords.java
@@ -3,5 +3,15 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class StopWords {
+    //Stop words declaration
+    File stopWords = new File("/Users/loganameres/Desktop/Programming Workshop/Semester-Project/stopwords.txt");
 
+        try (Scanner myReader = new Scanner(stopWords)) {
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+    } catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+    }
 }
