f581b263b0953401a4fcc83b71bf08423b5ef69f
willlandon <willlandon350@gmail.com>
2025-10-08 11:17:05 -0400

    file renames

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/headphones/article1.txt");
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/headphones/article2.txt");
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/headphones/article3.txt");
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/movies/guardian.txt");
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/movies/RogerEbert.txt");
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/movies/Woodstocker.txt");
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/roman/caesar.txt");
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/roman/caligula.txt");
-        File myObj = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/roman/constantine.txt");
+        File head1 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/headphones/article1.txt");
+        File head2 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/headphones/article2.txt");
+        File head3 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/headphones/article3.txt");
+        File movie1 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/movies/guardian.txt");
+        File movie2 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/movies/RogerEbert.txt");
+        File movie3 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/movies/Woodstocker.txt");
+        File roman1 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/roman/caesar.txt");
+        File roman2 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/roman/caligula.txt");
+        File roman3 = new File("/Users/michaelkobus/IdeaProjects/Semester-Project/roman/constantine.txt");
 
     }
 }
