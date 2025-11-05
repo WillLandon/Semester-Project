5e3109f26fdc3563a8d131abf29281cdfb91e99c
loganameres <ljameres@gmail.com>
2025-11-05 11:29:21 -0500

    added positive and negative words

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
