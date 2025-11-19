7c441ebf2966fb2c7dfabac3d5b942c4d2003e65
WillLandon <willlandon350@gmail.com>
2025-11-19 12:02:17 -0500

    Created GFG and fixed main a little.

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -9,33 +11,78 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String topic = "";
 
+        ArrayList<String> topics = new ArrayList<>();
+        topics.add("headphones");
+        topics.add("movies");
+        topics.add("roman");
+
+        //Get list of files for specified topic
+        ArrayList<File> topicFiles = getTopicFiles(topic);
+        //GFG.createMyFolders(topics);
         while (true) {
             System.out.println("Welcome to text analysis using Java!");
-            System.out.println("Please select a topic you would like to analyze: ");
-            System.out.println("1. headphones");
-            System.out.println("2. movies");
-            System.out.println("3. roman");
-            int option = scanner.nextInt();
-            switch (option) {
+            System.out.println("1. Select a topic");
+            System.out.println("2. Add a topic");
+            System.out.println("3. Add an Article");
+            int choice = scanner.nextInt();
+            switch (choice) {
                 case 1:
-                    topic = "headphones";
-                    break;
-                case 2:
-                    topic = "movies";
-                    break;
-                case 3:
-                    topic = "roman";
-                    break;
+                    //prints all topic options
+                    for (int i = 0; i < topics.size(); i++) {
+                        topic = topics.get(i);
+                        System.out.println(i + 1 + ". " + topic);
+                    }
+                    int descision = scanner.nextInt();
+
+                    //Processing and printing files stats
+                    int i = 1;
+                    for (File file : topicFiles) {
+                        System.out.println(i + ". " + file.getName());
+                        i++;
+                    }
+                    //Analyze text and get results
+                    //TextAnalysis results = processor.analyzeText(file);
+//                    if (results != null) {
+//                        //Print statistics of the text
+//                        results.printStatistics();
+//                    }
+//                    System.out.println("----------");
+//                    break;
+//                case 2:
+//
+//                    break;
+//                case 3:
+//
+//                    break;
                 default:
                     System.out.println("Invalid option!");
             }
             break;
         }
 
+
+
+//            int option = scanner.nextInt();
+//            switch (option) {
+//                case 1:
+//                    topic = "headphones";
+//                    break;
+//                case 2:
+//                    topic = "movies";
+//                    break;
+//                case 3:
+//                    topic = "roman";
+//                    break;
+//                default:
+//                    System.out.println("Invalid option!");
+//            }
+//            break;
+//        }
+
         //Creates TextProcessor instance for text analysis
         TextProcessor processor = new TextProcessor();
-        //Get list of files for specified topic
-        ArrayList<File> topicFiles = getTopicFiles(topic);
+
+
 
         //Process each file in the topic directory
         for (File file : topicFiles) {
